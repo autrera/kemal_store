48e2756a777eb97a717a507e7cebe86e0b1a0570
Aldo Utera <Kugel85@gmail.com>
2017-01-19 23:23:26 -0500

    Agregando macro para generar multiples rutas de un recurso

--- src/kemal_test.cr
+++ src/kemal_test.cr
@@ -1,17 +1,32 @@
 require "kemal"
 
-#logging false
+# logging false
 
 macro admin_render(view_file_path)
   render "#{{{ view_file_path }}}", "src/views/layouts/admin.ecr"
 end
 
+struct AdminController
+  def index
+    admin_render "src/views/admin/orders/index.ecr"
+  end
+end
+
+macro resource(resource_name, resource_name_single)
+  get "/{{resource_name}}" do
+    {{resource_name_single}}Controller.new.index
+  end
+
+end
+
+resource admin, Admin
+
 get "/" do
   "Hello World!"
 end
 
-get "/admin" do
-  admin_render "src/views/admin/render.ecr"
-end
+# get "/admin" do
+#   admin_render "src/views/admin/orders/index.ecr"
+# end
 
 Kemal.run
