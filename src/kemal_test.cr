9edaaa66081372b99e6bf6847fdb01fc268c413d
Aldo Utera <Kugel85@gmail.com>
2017-01-24 22:50:51 -0500

    Siguendo el curso de la aplicacion

--- src/kemal_test.cr
+++ src/kemal_test.cr
@@ -6,27 +6,26 @@ macro admin_render(view_file_path)
   render "#{{{ view_file_path }}}", "src/views/layouts/admin.ecr"
 end
 
-struct AdminController
+struct OrderController
   def index
     admin_render "src/views/admin/orders/index.ecr"
   end
 end
 
-macro resource(resource_name, resource_name_single)
-  get "/{{resource_name}}" do
-    {{resource_name_single}}Controller.new.index
+macro resource(resource_route, resource_object)
+  get "/{{resource_route}}" do
+    {{resource_object}}Controller.new.index
   end
 
+  get "/{{resource_route}}" do
+    {{resource_object}}Controller.new.index
+  end
 end
 
-resource admin, Admin
+resource 'admin/orders', Order
 
 get "/" do
   "Hello World!"
 end
-
-# get "/admin" do
-#   admin_render "src/views/admin/orders/index.ecr"
-# end
 
 Kemal.run
