59075e59ea09fb8b0cf1041541878126319bc29d
Aldo Utera <Kugel85@gmail.com>
2017-01-30 21:43:34 -0500

    Agregando campos base a la tabla

--- db/migrations/20170129194338_add_products_table.sql
+++ db/migrations/20170129194338_add_products_table.sql
@@ -1,6 +1,16 @@
 -- +micrate Up
 -- SQL in section 'Up' is executed when this migration is applied
-
+CREATE TABLE products(
+  id INT PRIMARY KEY,
+  organization_id INT NOT NULL,
+  name VARCHAR NOT NULL,
+  sku VARCHAR NOT NULL,
+  stock INT NOT NULL,
+  price INT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
 
 -- +micrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE products;
