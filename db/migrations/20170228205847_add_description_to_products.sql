ba586d2af298d5001d3f2c1ad69cf142888e899e
Aldo Utrera <Kugel85@gmail.com>
2017-03-21 11:10:06 -0500

    Seteando valor inicial a la nueva columna

--- db/migrations/20170228205847_add_description_to_products.sql
+++ db/migrations/20170228205847_add_description_to_products.sql
@@ -1,7 +1,9 @@
 -- +micrate Up
 -- SQL in section 'Up' is executed when this migration is applied
 
-ALTER TABLE products ADD COLUMN description TEXT DEFAULT NULL;
+ALTER TABLE products ADD COLUMN description TEXT;
+UPDATE products SET description = "";
+ALTER TABLE products ALTER COLUMN description SET NOT NULL;
 
 -- +micrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
