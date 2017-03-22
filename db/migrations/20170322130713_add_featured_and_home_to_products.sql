d75eea9d29e3a047e5292133e6cff379c00728ae
Aldo Utrera <Kugel85@gmail.com>
2017-03-22 13:10:53 -0500

    Add featured and in home columns to the products

---
+++ db/migrations/20170322130713_add_featured_and_home_to_products.sql
@@ -0,0 +1,16 @@
+-- +micrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+
+ALTER TABLE products ADD COLUMN featured BOOLEAN;
+UPDATE products SET featured = "f";
+ALTER TABLE products ALTER COLUMN featured SET NOT NULL;
+
+ALTER TABLE products ADD COLUMN in_home BOOLEAN;
+UPDATE products SET in_home = "f";
+ALTER TABLE products ALTER COLUMN in_home SET NOT NULL;
+
+-- +micrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
+
+ALTER TABLE products DROP COLUMN featured RESTRICT;
+ALTER TABLE products DROP COLUMN in_home RESTRICT;
