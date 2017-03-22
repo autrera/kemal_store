288c05365593821c731c2712ddf9dd3ce03ca797
Aldo Utrera <Kugel85@gmail.com>
2017-03-22 16:52:38 -0500

    Set default for new columns and fix default value

--- db/migrations/20170322130713_add_featured_and_home_to_products.sql
+++ db/migrations/20170322130713_add_featured_and_home_to_products.sql
@@ -2,12 +2,14 @@
 -- SQL in section 'Up' is executed when this migration is applied
 
 ALTER TABLE products ADD COLUMN featured BOOLEAN;
-UPDATE products SET featured = "f";
+UPDATE products SET featured = 'f';
 ALTER TABLE products ALTER COLUMN featured SET NOT NULL;
+ALTER TABLE products ALTER COLUMN featured SET DEFAULT FALSE;
 
 ALTER TABLE products ADD COLUMN in_home BOOLEAN;
-UPDATE products SET in_home = "f";
+UPDATE products SET in_home = 'f';
 ALTER TABLE products ALTER COLUMN in_home SET NOT NULL;
+ALTER TABLE products ALTER COLUMN in_home SET DEFAULT FALSE;
 
 -- +micrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
